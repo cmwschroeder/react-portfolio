7ee81250dbee7381b5111a45d1b185fa349e0fa8
cmwschroeder <cmwschroeder@gmail.com>
2022-05-30 23:02:45 -0700

    changed class to classname, added contact id to contact section

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Project({ projImg, projName, gitLink, depLink }) {
     return (
-        <div class="card m-auto lg:m-2 lg:w-2/5 bg-base-100 border-2 border-black">
-            <figure><img src={projImg} class="h-96" alt="Project preview" /></figure>
+        <div className="card m-auto lg:m-2 lg:w-2/5 bg-base-100 border-2 border-black">
+            <figure><img src={projImg} className="h-96" alt="Project preview" /></figure>
 
-            <h3 class="card-title m-3">{projName}</h3>
-            <div class="card-actions justify-around m-3">
-                <a href={depLink} target="_blank" class="btn btn-primary" rel="noreferrer">Visit Site</a>
-                <a href={gitLink} target="_blank" class="btn btn-secondary" rel="noreferrer">Visit Repo</a>
+            <h3 className="card-title m-3">{projName}</h3>
+            <div className="card-actions justify-around m-3">
+                <a href={depLink} target="_blank" className="btn btn-primary" rel="noreferrer">Visit Site</a>
+                <a href={gitLink} target="_blank" className="btn btn-secondary" rel="noreferrer">Visit Repo</a>
             </div>
         </div>
     )
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,38 +2,38 @@ import React from 'react';
 
 function Contact() {
     return (
-        <div class="w-full h-full flex justify-center">
-            <div class="card w-3/4 bg-base-100 shadow-xl my-6">
-                <div class="card-body w-full">
+        <div id="contact" className="w-full h-full flex justify-center">
+            <div className="card w-3/4 bg-base-100 shadow-xl my-6">
+                <div className="card-body w-full">
 
-                    <h2 class="card-title text-4xl">Contact Me:</h2>
+                    <h2 className="card-title text-4xl">Contact Me:</h2>
 
-                    <div class="form-control w-5/6 mx-auto">
-                        <label class="label">
-                            <span class="label-text text-2xl">Name: </span>
+                    <div className="form-control w-5/6 mx-auto">
+                        <label className="label">
+                            <span className="label-text text-2xl">Name: </span>
                         </label>
-                        <label class="input-group">
-                            <span class="hidden lg:flex lg:w-1/6">Name</span>
-                            <input type="text" placeholder="Name" class="input input-bordered w-full lg:w-11/12" id="name" />
+                        <label className="input-group">
+                            <span className="hidden lg:flex lg:w-1/6">Name</span>
+                            <input type="text" placeholder="Name" className="input input-bordered w-full lg:w-11/12" id="name" />
                         </label>
                     </div>
 
-                    <div class="form-control w-5/6 mx-auto">
-                        <label class="label">
-                            <span class="label-text text-2xl">Email: </span>
+                    <div className="form-control w-5/6 mx-auto">
+                        <label className="label">
+                            <span className="label-text text-2xl">Email: </span>
                         </label>
-                        <label class="input-group">
-                            <span class="hidden lg:flex lg:w-1/6">Email</span>
-                            <input type="text" placeholder="Email" class="input input-bordered w-full lg:w-11/12" id="email" />
+                        <label className="input-group">
+                            <span className="hidden lg:flex lg:w-1/6">Email</span>
+                            <input type="text" placeholder="Email" className="input input-bordered w-full lg:w-11/12" id="email" />
                         </label>
                     </div>
 
-                    <div class="form-control w-5/6 mx-auto">
-                        <label class="label">
-                            <span class="label-text text-2xl">Message: </span>
+                    <div className="form-control w-5/6 mx-auto">
+                        <label className="label">
+                            <span className="label-text text-2xl">Message: </span>
                         </label>
-                        <label class="input-group">
-                            <textarea class="textarea textarea-bordered h-48 w-full" placeholder="Message"
+                        <label className="input-group">
+                            <textarea className="textarea textarea-bordered h-48 w-full" placeholder="Message"
                                 id="message"></textarea>
                         </label>
                     </div>
