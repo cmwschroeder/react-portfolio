ced1c5a1f6ea6ab938df7d98831e846524d97930
cmwschroeder <cmwschroeder@gmail.com>
2022-05-31 22:14:44 -0700

    added images to src folder, changed links to images

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import background from '../images/black-and-gold-background.jpg';
+import my_picture from '../images/my_picture.jpeg';
 
 const styles = {
     heroStyle: {
-        backgroundImage: "url('/images/black-and-gold-background.jpg')",
+        backgroundImage: `url(${background})`,
     }
 }
 
@@ -10,7 +12,7 @@ function Hero() {
     return (
         <section className="hero min-1/2" style={styles.heroStyle}>
             <div className="hero-content flex-col lg:flex-row">
-                <img src="/images/my_picture.jpeg" className="sm:max-w-md rounded-lg shadow-2xl" alt="Me" />
+                <img src={my_picture} className="sm:max-w-md rounded-lg shadow-2xl" alt="Me" />
                 <div>
                     <h1 className="text-5xl font-bold">Christian Schroeder</h1>
                 </div>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Project from '../Project';
+import pethub from '../../images/pethub.png'
+import artketplace from '../../images/artketplace.png'
+import weather_dashboard from '../../images/weather-dashboard.png'
+import tech_blog from '../../images/tech-blog.png'
+import code_quiz from '../../images/code-quiz.png'
+import note_taker from '../../images/note-taker.png'
 
 function Projects() {
     return (
@@ -8,12 +14,12 @@ function Projects() {
                 <div className="card-body w-5/6 m-auto">
                     <h2 className="text-3xl">Projects: </h2>
                     <div className="flex flex-col sm:flex-row sm:flex-wrap sm:justify-around">
-                        <Project projImg="/images/pethub.png" projName="Pethub" gitLink="https://github.com/cmwschroeder/pethub" depLink="https://cmwschroeder.github.io/pethub/" />
-                        <Project projImg="/images/artketplace.png" projName="ARTketplace" gitLink="https://github.com/cmwschroeder/artketplace" depLink="https://artketplace.herokuapp.com/" />
-                        <Project projImg="/images/weather-dashboard.png" projName="Weather Dashboard" gitLink="https://github.com/cmwschroeder/weather-dashboard" depLink="https://cmwschroeder.github.io/weather-dashboard/" />
-                        <Project projImg="/images/tech-blog.png" projName="Tech Blog" gitLink="https://github.com/cmwschroeder/tech-blog" depLink="https://safe-mountain-78939.herokuapp.com/" />
-                        <Project projImg="/images/code-quiz.png" projName="Code Quiz" gitLink="https://github.com/cmwschroeder/code-quiz" depLink="https://cmwschroeder.github.io/code-quiz/" />
-                        <Project projImg="/images/note-taker.png" projName="Note Taker" gitLink="https://github.com/cmwschroeder/note-taker" depLink="https://intense-springs-41633.herokuapp.com/" />
+                        <Project projImg={pethub} projName="Pethub" gitLink="https://github.com/cmwschroeder/pethub" depLink="https://cmwschroeder.github.io/pethub/" />
+                        <Project projImg={artketplace} projName="ARTketplace" gitLink="https://github.com/cmwschroeder/artketplace" depLink="https://artketplace.herokuapp.com/" />
+                        <Project projImg={weather_dashboard} projName="Weather Dashboard" gitLink="https://github.com/cmwschroeder/weather-dashboard" depLink="https://cmwschroeder.github.io/weather-dashboard/" />
+                        <Project projImg={tech_blog} projName="Tech Blog" gitLink="https://github.com/cmwschroeder/tech-blog" depLink="https://safe-mountain-78939.herokuapp.com/" />
+                        <Project projImg={code_quiz} projName="Code Quiz" gitLink="https://github.com/cmwschroeder/code-quiz" depLink="https://cmwschroeder.github.io/code-quiz/" />
+                        <Project projImg={note_taker} projName="Note Taker" gitLink="https://github.com/cmwschroeder/note-taker" depLink="https://intense-springs-41633.herokuapp.com/" />
                     </div>
                 </div>
             </div>
